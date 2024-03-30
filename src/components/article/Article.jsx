23c7dcc7ede6bbc3549d3c72f3aa78caf7fdab5c
shivam shrivastava <shivamshrivaastava@gmail.com>
2024-03-30 22:29:36 +0530

    first-commit

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,25 @@
+import './article.css';
+import PropTypes from 'prop-types';
+
+const Article = ({ imgUrl, date, text }) => (
+  <div className='gpt3__blog-container_article'>
+    <div className='gpt3__blog-container_article-image'>
+      <img src={imgUrl} alt='blog_image' />
+    </div>
+    <div className="gpt3__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p>Read Full Article</p>
+    </div>
+  </div>
+);
+
+Article.propTypes = {
+  imgUrl: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+export default Article;
